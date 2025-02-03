e46734e1e3bfb946f00065a7c86ebd43db3d7949
Andy Mai <112735674+amai-21@users.noreply.github.com>
2025-02-02 21:26:45 -0600

    Moving on to Input Data handling

--- P0/main.cpp
+++ P0/main.cpp
@@ -2,21 +2,39 @@
 #include <iostream>
 #include <fstream> // For handling an input file from within the command line.
 #include <string> // For using getline(), specifically for handling keyboard input from the command line.
+#include <cctype> // For using isalnum() function to check if a character string is a letter or number.
 #include "buildTree.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]){
+
+	//----------------------------Handling Command Line Arguments----------------------------------------------------------------------------->
+	
+	//Declare a file object for reading:
+	ifstream fileName;
+
 	//Command line argument for if file was provided - Read from the file.
 	if (argc == 2) { //Resource: W3 Schools - C++ Files
-		ifstream filename(argv[1]);
-
+		//ifstream filename(argv[1]);
+		fileName.open(argv[1]);
 		//Test for handling a filename argument
 		//string testString;
-		//while (getline (filename, testString)) {
+		//while (getline (fileName, testString)) {
 		//	cout << testString;
 		//}
-		filename.close();
+
+		//TODO traverse through the file. If there is a non-approved character, abort the program and display an appropriate error message.
+		string fileNameString; 
+		while(getline(fileName, fileNameString)) {
+			if (!isalnum(fileNameString) || fileNameString < 33 || fileNameString > 43) {
+				cout << "Detected a character that wasn't valid." << endl;
+				cout << "Character: " << fileNameString << endl;
+				cout << "Program will now abort." << endl;
+				exit(0);
+			}
+		}
+		
 	} else if(argc == 1) { // Read from keyboard until simulated keyboard EOF
 		ofstream tempFilename("keyboardInputFile.txt");
 		string keyboardReadingText;
@@ -25,11 +43,11 @@ int main(int argc, char* argv[]){
 		cout << "Enter a string. To stop, please press 'CTRL + D': ";
 		while(getline(cin, keyboardReadingText)){ 
 			cout << "Enter another string or press 'CTRL + D' to quit: ";
+			//TODO Check if the keyboard input is an approved character string. If it isn't, then abort the program and display an appropriate error message.
 
 			tempFilename << keyboardReadingText;
 			tempFilename << " "; // To delimit words by one space.	
 		}
-		//TODO: Read from tempFilename. If there are more than three strings, display an error message and quit.
 	} else if(argc > 2) { // If user input 3 or more strings, display an error:
 		cout << "P0 ";
 		for (int i = 0; i < argc - 1; i++) {
@@ -40,6 +58,10 @@ int main(int argc, char* argv[]){
 		cout << "Usage: P0 [filename]" << endl;
 		exit(0);
 	}
+
+	fileName.close();
+	//-----------------------------------------------------Input Data Validation---------------------------------------------------------------->
+	
 
 	return 0;
 }
--- testingDirectory/main.cpp
+++ testingDirectory/main.cpp
@@ -5,43 +5,24 @@
 
 using namespace std;
 
+int getASCII(char);
+
 int main(int argc, char* argv[]){
+	char ch = 'A';
 
-	string myText;
-	if (argc > 1) {
-		ifstream MyReadFile(argv[1]);
-		cout << argv[1];
-		while (getline (MyReadFile, myText)) {
-			cout << myText;
-		}
-		MyReadFile.close();
-	}
-	//cout << argv[0];
+	cout << (getASCII(ch));
 	
-	//Testing creating files and writing input text into a file:
-	if (argc == 1){
-		ofstream MyFile("creatingTestFile.txt");
-		cout << "Enter some text and we will write the text into 'creatingTestFile.txt'";
-		//cin >> myText;
-		
-		int counter = 1;
-		while (argv[counter] != '\0') {
-			counter++;
-			getline(cin, argv[counter]);
-
-		}
-
-		//test cases:
-		int counter2 = 1;
-		while ()
-
-		//MyFile << myText;
-
-		//cout << myText;
-	}
-
 
 
 	
 	return 0;
+}
+
+int getASCII(char ch) {
+	if (ch == 65) {
+		return 65;
+	} else {
+		return 0;
+	}
+	
 }
