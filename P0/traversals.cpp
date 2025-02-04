455c0e215e78fe23c1392f37de33a1c6909d6462
Andy Mai <112735674+amai-21@users.noreply.github.com>
2025-02-04 17:20:33 -0600

    Implemented Level-Order Traversal with a helper function.

--- P0/traversals.cpp
+++ P0/traversals.cpp
@@ -1,7 +1,29 @@
 //traversals.cpp
 #include "traversals.h"
 
-void traverseLevelOrder(node_t*, const char[]){
+// Helper function for recursive level order traversal:
+void levelOrderRec(node_t*, root, int level, vector<vector<int>>& res){
+	// Base case: If node is null, return
+	if (root == nullptr) return;
+
+	// Add a new level to the result if needed.
+	if (res.size() <= level)
+		res.push_back({});
+
+	// Add current node's data to its corresponding level
+	res[level].push_back(root->data);
+
+	// Recur for left and right children
+	levelOrderRec(root->left, level + 1, res);
+	levelOrderRec(root->right, level + 1, res);
+}
+
+void traverseLevelOrder(node_t* root){
+	// Stores the result level by level
+	vector<vector<int>> res;
+
+	levelOrderRec(root, 0, res);
+	return res;
 	
 }
 
