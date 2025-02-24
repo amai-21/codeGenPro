458b7d3afc637d427f56a83075f62fade96ae365
amai-21 <andrew.y.mai@gmail.com>
2025-02-24 14:48:18 -0600

    Created a directory called 'P1', made copies of files in P0 and sent them to P1, and downloaded the driver table that will be implemented in P1.

---
+++ P1/buildTree.cpp
@@ -0,0 +1,38 @@
+//buildTree.cpp
+#include <iostream>
+#include <string>
+#include <vector>
+#include "buildTree.h"
+#include "node.h"
+
+node_t* GetNewNode(string data, string::size_type charCount, vector<string> stringsSeen) {
+//node_t* GetNewNode(string::size_type charCount, vector<string> stringsSeen) {
+	node_t* newNode = new node_t();
+	newNode->data = data;
+	newNode->charCount = charCount;
+	newNode->left = newNode->right = NULL;
+	newNode->stringsSeen = stringsSeen;
+	return newNode;
+}
+
+//Function that recursively calls itself when inserting a new node.
+//node_t* Insert(node_t* root, string data, int charCount) { // empty tree
+node_t* Insert(node_t* root, string data, string::size_type charCount, vector<string> stringsSeen) {	
+//node_t* Insert(node_t* root, string::size_type charCount, vector<string> stringsSeen) {
+
+	if (root == NULL) { // base case
+		root = GetNewNode(data, charCount, stringsSeen);
+		//root = GetNewNode(charCount, stringsSeen);	
+	}
+//	else if (data <= root->data) {
+	else if (charCount < root->charCount) { // Store in left sub tree
+		root->left = Insert(root->left, data, charCount, stringsSeen);
+	}
+	else if(charCount > root->charCount) { // Store in right subtree 
+		root->right = Insert(root->right, data, charCount, stringsSeen);
+
+	} else if (charCount == root->charCount) { // Append next word to old word (root->data)
+		root->data += " " + data;	
+	}
+	return root;
+}
