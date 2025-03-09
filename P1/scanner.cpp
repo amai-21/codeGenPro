4973cad9c2435ba8eae96870b25636684722963d
amai-21 <andrew.y.mai@gmail.com>
2025-03-09 16:48:47 -0500

    Finished mapping out token type t1 and finished implementing a scanner for token type t2. All updates in scanner.cpp

--- P1/scanner.cpp
+++ P1/scanner.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <iostream>
 #include "scanner.h"
 
 using namespace std;
@@ -18,7 +19,42 @@ private:
 		typet1["\""] = TokenType::TYPET1;
 		typet1["#"] = TokenType::TYPET1;
 		typet1["$"] = TokenType::TYPET1;
+		typet1["%"] = TokenType::TYPET1;
+		typet1["&"] = TokenType::TYPET1;
+		typet1["'"] = TokenType::TYPET1;
+		typet1["("] = TokenType::TYPET1;
+		typet1[")"] = TokenType::TYPET1;
+	}
+	
+	// Function to check if a character is a digit.
+	bool isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+
+	string getNextToken() {
+
+		size_t start = position;
+		// Check for type t2 tokens:
+		if (input[position] == '+') {
+			position++;
+			
+			// Check if next character is a digit:
+			if (isDigit(input[position])) {
+				// Keep going.	
+				position++; 
+			} else {
+				cout << "Invalid Token Type! This character does not belong to type T2 tokens: " << input[position] << endl;
+			        cout << "Program ending..." << endl;
+		       		exit(0);		       
+			}
+
+			while (position < input.length() && isDigit(input[position])) {
+				position++;
+			}		
+			return input.substr(start, position - start);	
+		}	
 	}	
+
 public:
 	
 
