e86b40243937e56ec167746633c1fad86e3623a8
amai-21 <andrew.y.mai@gmail.com>
2025-04-20 13:27:51 -0500

    Finished insertFunction. File(s) edited: staticSemantics.cpp

--- P3/staticSemantics.cpp
+++ P3/staticSemantics.cpp
@@ -1,13 +1,23 @@
 // staticSemantics.cpp
 #include "staticSemantics.h"
 #include <unordered_set>
-using namespace std;
+#include <iostream>
+using namespace std ;
 
+/* Unordered set code is kindly and educationally borrowed by GeeksforGeeks.org
+	URL: https://www.geeksforgeeks.org/unordered_set-in-cpp-stl/
+*/
 // Global Symbol Table:
 unordered_set<string> symbolTable;
 
 void insertFunction(string symbolString){
+	if (symbolTable.count(symbolString) > 0) { // Check if there already is an occurrence of the string in the table.
+		cerr << "ERROR! Attempted to redefine a varaible: " << symbolString << endl;
+		exit(1);	
+	}
 
+	// Otherwise, insert the symbolString
+	symbolTable.insert(symbolString); 
 }
 
 bool verify(string symbolString){
